f33d1f8724dd03d2c30aa47e89e45a2f595beb19
v1ktorbro <vitya.abrosimoff2016@ya.ru>
2022-07-05 14:15:03 +0500

    feat(App): connect Main component

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import Header from '../Header/Header';
 import Footer from '../Footer/Footer';
+import Main from '../Main/Main';
 
 function App() {
   return (
     <>
       <Header />
+      <Main />
       <Footer />
     </>
   );
