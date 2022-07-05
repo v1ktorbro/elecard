f4dc10096743ee8c2f721341a70ab780a66c83dd
v1ktorbro <vitya.abrosimoff2016@ya.ru>
2022-07-05 14:16:46 +0500

    feat(Main): create and connect navigation with section cards

---
+++ src/components/Main/Main.js
@@ -0,0 +1,14 @@
+import './Main.css';
+import Navigation from '../Navigation/Navigation';
+import SectionCards from '../SectionCards/SectionCards';
+
+function Main() {
+  return (
+    <main className='main'>
+      <Navigation />
+      <SectionCards />
+    </main>
+  );
+}
+
+export default Main;
