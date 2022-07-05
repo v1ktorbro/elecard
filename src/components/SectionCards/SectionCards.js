8ddca96aecf481feba0a63cd0c7af6d773936d3c
v1ktorbro <vitya.abrosimoff2016@ya.ru>
2022-07-05 14:17:53 +0500

    feat(SectionCards): connect, create and layout

---
+++ src/components/SectionCards/SectionCards.js
@@ -0,0 +1,21 @@
+import './SectionCards.css';
+import Card from '../Card/Card';
+
+function SectionCards() {
+  return (
+    <section className='section-cards'>
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+    </section>
+  );
+}
+
+export default SectionCards;
