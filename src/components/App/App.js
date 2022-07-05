bec3fda61a487c9040076ffe3f4513296cab4de3
v1ktorbro <vitya.abrosimoff2016@ya.ru>
2022-07-05 10:33:27 +0500

    feat(App): connect Header and Footer

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,12 +1,13 @@
 import './App.css';
+import Header from '../Header/Header';
+import Footer from '../Footer/Footer';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>hello</h1>
-      </header>
-    </div>
+    <>
+      <Header />
+      <Footer />
+    </>
   );
 }
 
