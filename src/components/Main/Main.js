c9682aa079d16c04a2775a81aa1347b05bfc0fa1
v1ktorbro <vitya.abrosimoff2016@ya.ru>
2022-07-05 18:41:24 +0500

    feat(Main): connect Paginate component

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,12 +1,14 @@
 import './Main.css';
 import Navigation from '../Navigation/Navigation';
 import SectionCards from '../SectionCards/SectionCards';
+import Pagination from '../Pagination/Pagination';
 
 function Main() {
   return (
     <main className='main'>
       <Navigation />
       <SectionCards />
+      <Pagination />
     </main>
   );
 }
