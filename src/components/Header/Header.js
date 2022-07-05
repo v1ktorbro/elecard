37750c73f3802a87742a5c0069fed020e15cd0df
v1ktorbro <vitya.abrosimoff2016@ya.ru>
2022-07-06 01:19:38 +0500

    refactor(Header): rename button

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,7 +4,7 @@ function Header() {
   return (
     <header className='header'>
       <a href='https://www.elecard.com/ru' target={'_blank'} rel={'noreferrer'} className='header__logo' />
-      <button type='reset' className='header__btn-reset'>Сбросить настройки</button>
+      <button type='reset' className='header__btn-reset'>Сбросить</button>
     </header>
   );
 }
