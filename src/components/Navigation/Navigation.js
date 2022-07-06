46cf6d6fc7a3d99d40571bfac5bab8d86a0c2adc
v1ktorbro <vitya.abrosimoff2016@ya.ru>
2022-07-06 06:29:33 +0500

    feat(Navigation): add background color handler checked or not input

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,27 +1,65 @@
 import './Navigation.css';
 
 function Navigation() {
+  
+  const handlerClick = (evt) => {
+    const {value} = evt.target;
+    const allInputs = document.querySelectorAll('.navigation__input');
+    allInputs.forEach((inputNode) => {
+      const setterStyleContainerInput = (input, value) => {
+        const containerInput = input.closest('.navigation__input-wrapper');
+        containerInput.style.backgroundColor = value;
+      };
+      if (inputNode.value !== value) inputNode.checked = false;
+      (inputNode.value === value && inputNode.checked) ? setterStyleContainerInput(inputNode, '#c3e5ff') : setterStyleContainerInput(inputNode, 'transparent');
+    });
+  };
+
   return (
     <nav className='navigation'>
       <h3 className='navigation__title'>Отсортировать по:</h3>
       <form className='navigation__form-chekbox'>
         <div className='navigation__input-wrapper'>
-          <input className={`navigation__input navigation__input_category`} id='category' type='checkbox' />
+          <input
+            onClick={handlerClick} 
+            className={`navigation__input navigation__input_category`} 
+            value='category' 
+            id='category' 
+            type='checkbox' 
+          />
           <label className='navigation__label-input navigation__label-input_category' htmlFor='category'>категории</label>
         </div>
 
         <div className='navigation__input-wrapper'>
-          <input className={`navigation__input navigation__input_date`} id='date' type='checkbox' />
+          <input 
+            onClick={handlerClick} 
+            className={`navigation__input navigation__input_date`} 
+            value='date' 
+            id='date' 
+            type='checkbox' 
+          />
           <label className='navigation__label-input navigation__label-input_date' htmlFor='date'>дате</label>
         </div>
 
         <div className='navigation__input-wrapper'>
-          <input className={`navigation__input navigation__input_name`} id='name' type='checkbox' />
+          <input 
+            onClick={handlerClick} 
+            className={`navigation__input navigation__input_name`} 
+            value='name' 
+            id='name'
+            type='checkbox' 
+          />
           <label className='navigation__label-input navigation__label-input_name' htmlFor='name'>названию файла</label>
         </div>
 
         <div className='navigation__input-wrapper'>
-          <input className={`navigation__input navigation__input_size`} id='size' type='checkbox' />
+          <input 
+            onClick={handlerClick} 
+            className={`navigation__input navigation__input_size`} 
+            value='size' 
+            id='size'
+            type='checkbox' 
+          />
           <label className='navigation__label-input navigation__label-input_size' htmlFor='size'>размеру файла</label>
         </div>
       </form>
