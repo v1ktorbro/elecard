db0b72d74319133511becbfac58e719973b4c3bb
v1ktorbro <vitya.abrosimoff2016@ya.ru>
2022-07-07 22:29:20 +0500

    feat(SectionCard): render data from api

--- src/components/SectionCards/SectionCards.js
+++ src/components/SectionCards/SectionCards.js
@@ -1,19 +1,24 @@
 import './SectionCards.css';
+import React from 'react';
 import Card from '../Card/Card';
+import { useSelector, useDispatch } from 'react-redux';
+import { fetchCards } from '../../asyncAction/cards';
 
 function SectionCards() {
+  const dispatch = useDispatch();
+  const cards = useSelector((state) => state.cards.cards);
+
+  React.useEffect(() => {
+    dispatch(fetchCards());
+  }, []);
+
   return (
     <section className='section-cards'>
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
+      {cards.map((card) => {
+        return (
+          <Card key={card.timestamp}  data={card}/>
+        );
+      })}
     </section>
   );
 }
