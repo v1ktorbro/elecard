0f80002c915ef618bffcdb5d08025eb9beb0907c
Viktor Abrosimov <vitya.abrosimoff2016@ya.ru>
2022-07-11 19:06:03 +0500

    refactor(App): transfer the loading of initial cards from the SectionCards components into App

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,8 +2,16 @@ import './App.css';
 import Header from '../Header/Header';
 import Footer from '../Footer/Footer';
 import Main from '../Main/Main';
+import { useDispatch } from 'react-redux';
+import { fetchCards } from '../../asyncAction/cards';
+import React from 'react';
 
 function App() {
+  const dispatch = useDispatch();
+
+  React.useEffect(() => {
+    dispatch(fetchCards());
+  }, []);
   return (
     <>
       <Header />
