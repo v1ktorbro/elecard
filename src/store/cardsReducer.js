c646430857e4492a9b698c99a74574971ee9eebc
Viktor Abrosimov <vitya.abrosimoff2016@ya.ru>
2022-07-12 18:22:23 +0500

    refactor(cardReducer): semantic changes

--- src/store/cardsReducer.js
+++ src/store/cardsReducer.js
@@ -7,7 +7,7 @@ const GET_ALL_CARDS = 'GET_ALL_CARDS';
 export const cardsReducer = (state = defaultState, action) => {
   switch (action.type) {
     case GET_ALL_CARDS:
-      return {...state, cards: action.payload}
+      return {...state, cards: action.payload};
     default:
       return state;
   }
