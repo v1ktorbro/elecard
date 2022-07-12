260d3d807bac495a7209ae3e26793a398e799a37
Viktor Abrosimov <vitya.abrosimoff2016@ya.ru>
2022-07-12 16:31:02 +0500

    refactor(SectionCards): transfer logic load initalData to App component

--- src/components/SectionCards/SectionCards.js
+++ src/components/SectionCards/SectionCards.js
@@ -1,22 +1,15 @@
 import './SectionCards.css';
-import React from 'react';
 import Card from '../Card/Card';
-import { useSelector, useDispatch } from 'react-redux';
-import { fetchCards } from '../../asyncAction/cards';
+import { useSelector } from 'react-redux';
 
 function SectionCards() {
-  const dispatch = useDispatch();
   const cards = useSelector((state) => state.cards.cards);
-
-  React.useEffect(() => {
-    dispatch(fetchCards());
-  }, []);
 
   return (
     <section className='section-cards'>
       {cards.map((card) => {
         return (
-          <Card key={card.timestamp}  data={card}/>
+          <Card key={card.timestamp} data={card}/>
         );
       })}
     </section>
