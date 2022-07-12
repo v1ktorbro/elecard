80416a8ab7630cdce27b9d88b3fa88f2c8d59786
Viktor Abrosimov <vitya.abrosimoff2016@ya.ru>
2022-07-12 18:22:38 +0500

    feat(index): connect params reducer

--- src/store/index.js
+++ src/store/index.js
@@ -1,12 +1,14 @@
 import { createStore, combineReducers, applyMiddleware } from 'redux';
 import { sortReducer } from './sortReducer';
 import { cardsReducer } from './cardsReducer';
+import { paramsReducer } from './paramsReducer';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import thunk from 'redux-thunk';
 
 const rootReducer = combineReducers({
   sort: sortReducer,
   cards: cardsReducer,
+  params: paramsReducer,
 });
 
 export const store = createStore(rootReducer, composeWithDevTools((applyMiddleware(thunk))));
