d4c119227a291b1db4cccc864aeb63200b273b26
Viktor Abrosimov <vitya.abrosimoff2016@ya.ru>
2022-07-13 22:23:44 +0500

    feat(App): create handler uniq param category

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,17 +1,39 @@
 import './App.css';
+import React from 'react';
 import Header from '../Header/Header';
 import Footer from '../Footer/Footer';
 import Main from '../Main/Main';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { fetchCards } from '../../asyncAction/cards';
-import React from 'react';
+import { setParamCategory } from '../../store/paramsReducer';
 
 function App() {
   const dispatch = useDispatch();
+  const cards = useSelector((state) => state.cards.cards);
+
+  const setUniqParamCategory = (cards) => { 
+    let obj = {};
+    let categoryArr = [];
+    let count = 1;
+    cards.forEach((card) => {
+      if (!(card.category in obj)) obj[card.category] = count;
+      else obj[card.category] = count++;
+      return obj;
+    });
+    Object.keys(obj).forEach((nameCategory) => {
+      categoryArr.push(nameCategory);
+    });
+    return dispatch(setParamCategory(categoryArr));
+  };
+
+  React.useEffect(() => {
+    setUniqParamCategory(cards);
+  }, [cards]);
 
   React.useEffect(() => {
     dispatch(fetchCards());
   }, []);
+  
   return (
     <>
       <Header />
